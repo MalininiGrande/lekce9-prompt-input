a2e9e6181b08ba862b2d2bfb11f47695331fe831
Sandra Malinova <90518039+MalininiGrande@users.noreply.github.com>
2022-04-12 19:00:54 +0200

    BMI index

---
+++ bmi.js
@@ -0,0 +1,17 @@
+// funkce pro vzpocet BMI
+
+function vypocitejBMI() {
+    let vyskaMetry = parseFloat(document.querySelector("input[name=vyska]").value) / 100;
+    let vahaKilogramy = parseFloat(document.querySelector("input[name=vaha]").value);
+    let BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2));
+    console.log(BMI);
+}
+
+document.querySelectorAll('input').forEach((element) => {
+element.addEventListener('change', vypocitejBMI);
+});
+
+// separatni funkce pro zapsani BMI do UI
+function vypisBMI(BMI) {
+    document.querySelector("#vysledek > span").innerHTML = BMI;
+}
---
+++ prompt.js
@@ -0,0 +1,9 @@
+// vztvoreni promptu pro zadani jmena a prijmeni a veku a naplneni do promennych
+
+let celeJmeno = prompt("Zadej jmeno a prijmeni");
+let vek = prompt("Napis svuj vek", 18);
+
+// vypsani vysledku pomoci alert
+
+// alert('Jmenujes se ' + celeJmeno + " a je ti " + vek + " let");
+alert(`jmenujes se ${celeJmeno} a je ti ${vek} let.`)
